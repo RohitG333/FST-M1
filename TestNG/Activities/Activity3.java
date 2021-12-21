2fa331c61a28740b942ce670c10ddf01324db8fb
RohitG333 <95602609+RohitG333@users.noreply.github.com>
2021-12-21 17:24:54 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity3.java
@@ -0,0 +1,40 @@
+package TestNGactivities;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+
+public class Activity3 {
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.get("https://www.training-support.net/selenium/login-form");
+    }
+
+    @Test
+    public void loginTest() {
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        username.sendKeys("admin");
+        password.sendKeys("password");
+        driver.findElement(By.xpath("//button[text()='Log in']")).click();
+        String loginMessage = driver.findElement(By.id("action-confirmation")).getText();
+        Assert.assertEquals("Welcome Back, admin", loginMessage);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.close();
+    }
+}
